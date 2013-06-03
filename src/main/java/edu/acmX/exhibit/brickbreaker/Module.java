ecfc463e5b8a8e0222c01aa1b16e38ed79142367
Matthew Stech <mstech@mymail.mines.edu>
2013-06-03 10:32:23 -0600

    Added moving paddle

--- src/main/java/edu/acmX/exhibit/brickbreaker/Module.java
+++ src/main/java/edu/acmX/exhibit/brickbreaker/Module.java
@@ -4,11 +4,24 @@ import edu.mines.acmX.exhibit.module_manager.ProcessingModule;
 
 public class Module extends ProcessingModule {
 
+	public static int BACKGROUND_COLOR;
+	
+	private Paddle paddle;
+	
 	public void setup() {
-		
+		size(width, height);
+		BACKGROUND_COLOR = color(0, 0, 139);
+		paddle = new Paddle(this, width / 2, height - (height / 20), width / 12, height / 25);
+		noCursor();
 	}
 	
 	public void draw() {
-		
+		update();
+		background(BACKGROUND_COLOR);
+		paddle.draw();
+	}
+	
+	public void update() {
+		paddle.update();
 	}
 }

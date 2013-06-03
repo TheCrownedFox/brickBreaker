2772724eaeeb4005b5773bbf8dd42f015ff2fea6
Matthew Stech <mstech@mymail.mines.edu>
2013-06-03 09:42:59 -0600

    initial commit

---
+++ src/main/java/edu/acmX/exhibit/brickbreaker/Module.java
@@ -0,0 +1,14 @@
+package edu.acmX.exhibit.brickbreaker;
+
+import edu.mines.acmX.exhibit.module_manager.ProcessingModule;
+
+public class Module extends ProcessingModule {
+
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+}
